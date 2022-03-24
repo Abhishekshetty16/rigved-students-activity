ea1ddb89d9317363403ca60bd2b67df20bc34edf
abhishekshetty <abhishetty563@gmail.com>
2022-03-24 09:56:14 +0530

    as22

--- NodeJS/Day 22/activity2.js
+++ NodeJS/Day 22/activity2.js
@@ -4,7 +4,7 @@ let read=require("readline-sync");
  let id =read.question("Enter ID : ");
  let name=read.question("Enter your name : ");
  let salary=read.questionFloat("enter your salary : ");
- let z=[id ,name , salary];
+ let z={id ,name , salary};
  let jsonString=JSON.stringify(z);
  console.log(jsonString)
 
